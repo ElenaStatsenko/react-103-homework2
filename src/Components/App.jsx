4628807e8183ac172773b8b48193ec3c8a3d82d4
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2024-06-25 23:13:58 +0300

    6 задание

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,14 +2,32 @@ import Description from "./Description/Description";
 import Options from "./Options/Options";
 import Feedback from "./Feedback/Feedback";
 import Notification from "./Notification/Notification";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const initialObject = {
+  // начальный стейт
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
+const getLocalStorageData = () => {
+  const savedFeedback = JSON.parse(localStorage.getItem("saved-feedback"));
+
+  
+  if (savedFeedback !== null) {
+    return savedFeedback;
+  }
+  return initialObject;
+};
+
 export default function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+  const [feedback, setFeedback] = useState(getLocalStorageData);
+
+  console.log(useState(initialObject));
+
   const { good, neutral, bad } = feedback;
+
   const updateFeedback = (option) => {
     setFeedback({
       ...feedback,
@@ -18,23 +36,33 @@ export default function App() {
   };
 
   const totalFeedback = good + neutral + bad;
-  console.log(totalFeedback);
 
   const resetFeedback = () => {
     setFeedback({
-        good: 0,
-        neutral: 0,
-        bad: 0,  
+      good: 0,
+      neutral: 0,
+      bad: 0,
     });
-  }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("saved-feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
   return (
     <>
       <Description />
-      <Options onclickType={updateFeedback} options={feedback} onReset={resetFeedback} total={totalFeedback}/>
-{totalFeedback> 0 ?  <Feedback options={feedback} total={totalFeedback}/> :
-      <Notification/>}
-     
+      <Options
+        onclickType={updateFeedback}
+        options={feedback}
+        onReset={resetFeedback}
+        total={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback options={feedback} total={totalFeedback} />
+      ) : (
+        <Notification />
+      )}
     </>
   );
 }
-
